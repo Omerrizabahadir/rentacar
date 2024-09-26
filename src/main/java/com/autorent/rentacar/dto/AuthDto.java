c97bba754bc7e5e74f802d75560e6aea43a5c8f9
Omerrizabahadir <omrbahadir@gmail.com>
2024-09-26 18:43:16 +0300

    Create  AuthDto.java, a data container used for user authentication

---
+++ src/main/java/com/autorent/rentacar/dto/AuthDto.java
@@ -0,0 +1,12 @@
+package com.autorent.rentacar.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthDto {
+
+    private String email;
+    private String password;
+}
