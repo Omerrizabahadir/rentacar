848be4754a401085e287b9809f97e621ddd1a594
Omerrizabahadir <omrbahadir@gmail.com>
2024-09-26 18:39:59 +0300

    updated CustomerController and CustomerService

--- src/main/java/com/autorent/rentacar/controller/CustomerController.java
+++ src/main/java/com/autorent/rentacar/controller/CustomerController.java
@@ -1,5 +1,7 @@
 package com.autorent.rentacar.controller;
 
+import com.autorent.rentacar.dto.AuthDto;
+import com.autorent.rentacar.dto.LoginDto;
 import com.autorent.rentacar.model.Customer;
 import com.autorent.rentacar.service.CustomerService;
 import lombok.RequiredArgsConstructor;
@@ -20,5 +22,9 @@ public class CustomerController {
     @PostMapping("/register")
     public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer){
         return new ResponseEntity<>(customerService.createCustomer(customer), HttpStatus.CREATED);
+    }
+    @PostMapping("/login")
+    public ResponseEntity<LoginDto> login(@RequestBody AuthDto authDto){
+        return new ResponseEntity<>(customerService.login(authDto),HttpStatus.OK);
     }
 }
--- src/main/java/com/autorent/rentacar/service/CustomerService.java
+++ src/main/java/com/autorent/rentacar/service/CustomerService.java
@@ -1,10 +1,16 @@
 package com.autorent.rentacar.service;
 
+import com.autorent.rentacar.dto.AuthDto;
+import com.autorent.rentacar.dto.LoginDto;
 import com.autorent.rentacar.enums.RoleEnum;
 import com.autorent.rentacar.model.Customer;
 import com.autorent.rentacar.repository.CustomerRepository;
-import com.autorent.rentacar.util.CustomerMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +24,12 @@ public class CustomerService {
 
     private final CustomerRepository customerRepository;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtService jwtService;
+
     public Customer createCustomer(Customer customer){
 
         if(Objects.isNull(customer.getRoles())){
@@ -27,4 +39,11 @@ public class CustomerService {
         return customerRepository.save(customer);
     }
 
+    public LoginDto login(AuthDto authDto){
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authDto.getEmail(), authDto.getPassword()));
+        if(authentication.isAuthenticated()){
+            return jwtService.generateToken(authentication);
+        }
+        throw  new UsernameNotFoundException("Invalid user details.");
+    }
 }
