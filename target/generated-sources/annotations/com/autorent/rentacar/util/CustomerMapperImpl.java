1b3ab95928a84ba861980e10bf0e54441141238a
Omerrizabahadir <omrbahadir@gmail.com>
2024-09-30 21:29:02 +0300

    added mapstructure

---
+++ target/generated-sources/annotations/com/autorent/rentacar/util/CustomerMapperImpl.java
@@ -0,0 +1,31 @@
+package com.autorent.rentacar.util;
+
+import com.autorent.rentacar.dto.CustomerDto;
+import com.autorent.rentacar.model.Customer;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-09-27T19:42:03+0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.1 (Oracle Corporation)"
+)
+public class CustomerMapperImpl implements CustomerMapper {
+
+    @Override
+    public CustomerDto customerToCustomerDto(Customer customer) {
+        if ( customer == null ) {
+            return null;
+        }
+
+        CustomerDto customerDto = new CustomerDto();
+
+        customerDto.setId( customer.getId() );
+        customerDto.setEmail( customer.getEmail() );
+        customerDto.setFirstName( customer.getFirstName() );
+        customerDto.setLastName( customer.getLastName() );
+        customerDto.setAddress( customer.getAddress() );
+        customerDto.setRoles( customer.getRoles() );
+
+        return customerDto;
+    }
+}
