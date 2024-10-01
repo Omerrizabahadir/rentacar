5312e29022f201a1813dfb6f6dd0ff046857f66a
Omerrizabahadir <omrbahadir@gmail.com>
2024-10-01 14:27:03 +0300

    updated CarController.java

--- src/main/java/com/autorent/rentacar/controller/CarController.java
+++ src/main/java/com/autorent/rentacar/controller/CarController.java
@@ -2,7 +2,6 @@ package com.autorent.rentacar.controller;
 
 import com.autorent.rentacar.model.Car;
 import com.autorent.rentacar.service.CarService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -45,28 +44,31 @@ public class CarController {
     public ResponseEntity<List<Car>> getAllCarList() {
         return new ResponseEntity<>(carService.getAllCarList(), HttpStatus.OK);
     }
+
     @PutMapping(value = "/update", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<Car> updateCar(@RequestParam(value = "file", required = false) MultipartFile file,
-                                         @ModelAttribute Car car){
-        return new ResponseEntity<>(carService.updateCar(file,car),HttpStatus.OK);
+                                         @ModelAttribute Car car) {
+        return new ResponseEntity<>(carService.updateCar(file, car), HttpStatus.OK);
     }
+
     @DeleteMapping("{id}")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-    public ResponseEntity<Void> deleteCar(@PathVariable("id") Long id){
+    public ResponseEntity<Void> deleteCar(@PathVariable("id") Long id) {
         carService.deleteCar(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PutMapping("active/{id}")
-    @PreAuthorize("hasAuthority(ROLE_ADMIN)")
-    public ResponseEntity<Void> activeToRentalCar(@PathVariable("id") Long id){
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<Void> activeToRentalCar(@PathVariable("id") Long id) {
         carService.activeOrDeActiveToRentalCar(id, true);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
     @PutMapping("deActive/{id}")
-    @PreAuthorize("hasAuthority(ROLE_ADMIN)")
-    public ResponseEntity<Void> deActiveToRentalCar(@PathVariable("id") Long id){
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<Void> deActiveToRentalCar(@PathVariable("id") Long id) {
         carService.activeOrDeActiveToRentalCar(id, false);
         return new ResponseEntity<>(HttpStatus.OK);
     }
