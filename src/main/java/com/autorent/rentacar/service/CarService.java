1c9d90d26533e19aa869bee1a95b68ef18e3f030
Omerrizabahadir <omrbahadir@gmail.com>
2024-10-19 15:13:42 +0300

    updated CarService

--- src/main/java/com/autorent/rentacar/service/CarService.java
+++ src/main/java/com/autorent/rentacar/service/CarService.java
@@ -33,8 +33,8 @@ public class CarService {
 
         if (Objects.nonNull(file)) {
             String imagePath = saveFile(file, car.getModelName());
-            car.setActive(true); // Araba kullanılabilir
-            car.setIsRented(false); // Araba kiralanmamış
+            car.setActive(true);
+            car.setIsRented(false);
             car.setCarStatus(CarStatus.AVAILABLE);
             car.setImage(imagePath);
         } else {
@@ -60,6 +60,7 @@ public class CarService {
     }
 
     public List<Car> getCarListByBrandId(Long brandId) {
+
         return carRepository.findCarListByBrandId(brandId);
     }
 
